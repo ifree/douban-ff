117824e61867ddbcc28325f4a7b254f7bfac0488
Frei Zhang <ifree0@gmail.com>
2014-03-27 01:43:04 +0800

    from soft to hard

--- doubanfm.js
+++ doubanfm.js
@@ -1,1 +1,80 @@
-/Users/ifree/.vimperator/plugin/doubanfm.js
+let INFO = xml`
+<plugin name="doubafm helper" version="1.0"
+        href="http://programming4fun.com"
+        summary="control doubanfm player"
+        xmlns="http://com.programming4fun.common">
+    <author>Frei Zhang</author>
+    <license href="http://opensource.org/licenses/mit-license.php">MIT</license>
+    <project name="Vimperator" minVersion="2.0"/>
+    <p>
+        Just a simple tool for control doubanfm player golbally
+    </p>
+</plugin>`;
+
+(function(){
+    function execScriptInTabs(filter,domjs){
+        [].forEach.call(gBrowser.tabs,function(itm){
+	    var ctab=itm.linkedBrowser;
+	    if(filter(ctab)){
+	        var s=ctab.contentDocument.createElement('script')
+	        s.textContent=domjs;
+	        ctab.contentDocument.body.appendChild(s);
+	    }
+        });
+    }
+    
+    mappings.add(
+        [modes.NORMAL],
+        [",ds"],"skip",
+        function(){
+            execScriptInTabs(function(ctab){
+                return ctab.contentDocument.location.host.match("douban\.fm");
+            },"DBR.act('skip')");//skip,pause,love
+        }
+    );
+    mappings.add(
+        [modes.NORMAL],
+        [",db"],"ban",
+        function(){
+            execScriptInTabs(function(ctab){
+                return ctab.contentDocument.location.host.match("douban\.fm");
+            },"DBnnR.act('ban')");//skip,pause,love
+        }
+    );
+    mappings.add(
+        [modes.NORMAL],
+        [",dl"],"like",
+        function(){
+            execScriptInTabs(function(ctab){
+                return ctab.contentDocument.location.host.match("douban\.fm");
+            },"DBR.act('love')");//skip,pause,love
+        }
+    );
+    mappings.add(
+        [modes.NORMAL],
+        [",dp"],"pause",
+        function(){
+            execScriptInTabs(function(ctab){
+                return ctab.contentDocument.location.host.match("douban\.fm");
+            },"DBR.act('pause')");//skip,pause,love
+        }
+    );
+    mappings.add(
+        [modes.NORMAL],
+        [",dc"],"get current music",
+        function(){
+            execScriptInTabs(function(ctab){
+                return ctab.contentDocument.location.host.match("douban\.fm");
+            },"document.title=FM.getCurrentSongInfo().url");//= =            
+            [].forEach.call(gBrowser.tabs,function(itm){
+	    var ctab=itm.linkedBrowser;
+	    if(ctab.contentDocument.location.host.match("douban\.fm")){
+                Components.classes["@mozilla.org/widget/clipboardhelper;1"]
+                .getService(Components.interfaces.nsIClipboardHelper)
+                .copyString(ctab.contentTitle);
+	    }
+        });
+        }
+    );
+})();
+
--- init-firefox.el
+++ init-firefox.el
@@ -1,1 +1,76 @@
-/Users/ifree/.emacs.d/lisp/init-firefox.el
+;;; init-firefox.js --- A set of functions to control firefox
+
+;;; Commentary:
+;; use moz-repl to interactive firefox with Emacs
+
+;;; Code:
+
+(autoload 'inferior-moz-process "moz" "MozRepl process" t)
+
+(defun send-to-ff (content)
+  (comint-send-string (inferior-moz-process)
+                      content
+                      )
+  )
+
+(defun ff-exec-script-in-tabs (script url-regex) 
+  (send-to-ff
+   (format "
+(function(script,u){
+        [].forEach.call(gBrowser.tabs,function(itm){
+	    var ctab=itm.linkedBrowser;
+	    if(ctab.contentDocument.location.host.match(u)){
+	        var s=ctab.contentDocument.createElement('script')
+	        s.textContent=script;
+	        ctab.contentDocument.body.appendChild(s);
+	    }
+        });        
+    })(\"%s\",\"%s\");
+"
+script url-regex
+)
+   )
+  )
+
+(defun fm-exec (script)
+  (ff-exec-script-in-tabs script "douban\.fm")
+  )
+
+(defun fm-skip ()
+  (interactive)
+  (fm-exec "DBR.act('skip')")
+  )
+
+(defun fm-ban ()
+  (interactive)
+  (fm-exec "DBnnR.act('ban')")
+  )
+
+(defun fm-love ()
+  (interactive)
+  (fm-exec "DBR.act('love')")
+  )
+
+(defun fm-pause ()
+  (interactive)
+  (fm-exec "DBR.act('pause')")
+  )
+
+(defun fm-copy-song ()
+  (interactive)
+  (fm-exec "document.title=FM.getCurrentSongInfo().url")
+  (send-to-ff "
+[].forEach.call(gBrowser.tabs,function(itm){
+	    var ctab=itm.linkedBrowser;
+	    if(ctab.contentDocument.location.host.match('douban\.fm')){
+                Components.classes['@mozilla.org/widget/clipboardhelper;1']
+                .getService(Components.interfaces.nsIClipboardHelper)
+                .copyString(ctab.contentTitle);
+	    }});"
+))
+
+;;; init-firefox.js ends here
+
+(provide 'init-firefox)
+
+;;; init-firefox.el ends here
